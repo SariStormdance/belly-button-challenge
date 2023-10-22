496b946eecaa7a97ce0f871a43c1e8a0b135d6ff
Sari Rojas <sari.stormdance@gmail.com>
2023-10-22 09:23:04 -0500

    saving progress

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -5,7 +5,7 @@ d3.json('https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1
     var SampleIDs = data.names;
     var Dropdown = d3.select("#selDataset");
     for (id of SampleIDs) {
-        Dropdown.append("option").text(id).property ("value", id)
+        Dropdown.append("option").text(id).property("value", id)
     }; 
     //Create function for option change for SampleID.
     var metaData = data.metadata;
@@ -18,17 +18,41 @@ d3.json('https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1
         var Result = data.samples.filter(s=>s.id === SampleIDs)[0];
     console.log(Result);
     }
-    Top10(data.names[0]);
-    //Create bar graph.
-    var trace1 = [
-        {
-            x: ['SampleIDs'],
-            y: ['Result'],
-            type: 'bar'
-        }
-    ];
-    Plotly.newPlot('col-md-5', data);
-
-
-    
+    Top10(data.names[0]); 
 });
+
+function optionChanged (chooseOption) {
+    console.log(chooseOption);
+    d3.json('https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json').then((data) => {
+        console.log(data);
+    var Result = data.samples.filter(s=>s.id === chooseOption)[0];
+        console.log(Result);
+
+// Trace1 for the Data to create bar chart
+let trace1 = {
+    x: Top10.map(object => object.sample_values),
+    y: Top10.map(object => object.otu_ids),
+    // text: Top10.map(object => object.otu_labels),
+    name: "OTU",
+    type: "bar",
+    orientation: "h"
+  };
+  
+// Data array
+let traceData = [trace1];
+  
+// Apply a title to the layout
+let layout = {
+    title: "OTU Results",
+    margin: {
+      l: 100,
+      r: 100,
+      t: 100,
+      b: 100
+    }
+  };
+  
+  // Render the plot to the div tag with id "plot"
+  Plotly.newPlot("plot", traceData, layout);
+    });
+};
